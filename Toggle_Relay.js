3819003b08b410448c93c6c3e066fd6eb0a85df3
Nahshon Williams <819599+nahshonw@users.noreply.github.com>
2022-09-14 10:49:15 +0200

    Working CYP Relay toggle

---
+++ Toggle_Relay.js
@@ -0,0 +1,27 @@
+var net = require('net');
+
+var client = new net.Socket();
+
+
+client.connect(23, '192.168.33.142', function () {
+    console.log('Connected');
+    client.write('RELAY 1 CLOSE\x0d\x0a');
+    setTimeout(() => {
+        client.write('RELAY 1 OPEN\x0d\x0a');
+        setTimeout(() => {
+            client.write('RELAY 1 CLOSE\x0d\x0a');
+            client.destroy(); // kill client after server's response
+
+        }, 3000);
+    }, 3000);
+
+});
+
+client.on('data', function (data) {
+    console.log('Received: ' + data);
+});
+
+client.on('close', function () {
+    console.log('Connection closed');
+});
+
